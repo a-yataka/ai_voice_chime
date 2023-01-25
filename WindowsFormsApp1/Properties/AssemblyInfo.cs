da8989beb046fc219839d3ef7b875f6fa53807fc
a-yataka <japan.zaas@gmail.com>
2023-01-26 03:52:28 +0900

    exe名を修正

--- WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("WindowsFormsApp1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AI_Chime")]
+[assembly: AssemblyDescription("wav時報とA.I.voiceから音声を作成するオマケ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyCompany("a-yataka")]
+[assembly: AssemblyProduct("AI_Chime")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
